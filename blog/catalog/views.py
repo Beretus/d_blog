54768255573cf8875282ebb8b365382bd421a227
Mateusz Mindewicz <mindewiczm@gmail.com>
2023-09-07 18:01:19 +0200

    Added base_generic site to early view of posts

--- blog/catalog/views.py
+++ blog/catalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import CustomUser, Post, Comment, Like
 
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+            }
+
+    return render(request, 'base_generic.html', context=context)
