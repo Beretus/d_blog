3204107ae97b8f370ae8d81db65a90eafe34f325
Mateusz Mindewicz <mindewiczm@gmail.com>
2023-09-07 19:40:35 +0200

    Fixed posts

--- blog/catalog/models.py
+++ blog/catalog/models.py
@@ -45,6 +45,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('CustomUser', on_delete=models.CASCADE, null=False, related_name='user_posts')
     context = models.CharField(max_length=20000)
+    #likePost = models.ManyToManyField('LikePost', related_name='post_likes')
     def __str__(self):
         return self.title
 
@@ -56,7 +57,10 @@ class Comment(models.Model):
     author = models.ForeignKey('CustomUser', on_delete=models.CASCADE, null=False, related_name='user_comments')
     context = models.CharField(max_length=2000)
     date = models.DateField(default=timezone.now)
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, null=False)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    #likeComment = models.ManyToManyField('LikeComment', related_name='comment_likes')
+
     def __str__(self):
         return self.context
 
@@ -69,17 +73,16 @@ class Comment(models.Model):
 class LikePost(models.Model):
     author = models.ForeignKey('CustomUser', on_delete=models.CASCADE, null=False, related_name='user_post_likes')
     date = models.DateTimeField(default=timezone.now)
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.SET_NULL, null=True)  # Dodaj to pole 
 
     def __str__(self):
-        return f'{self.author} liked {self.post}'
+        return f'{self.author} liked'
 
 
 
 class LikeComment(models.Model):
     author = models.ForeignKey('CustomUser', on_delete=models.CASCADE, null=False, related_name='user_comment_likes')
     date = models.DateTimeField(default=timezone.now)
-    comment = models.ForeignKey('Comment', on_delete=models.CASCADE)
-
+    comment = models.ForeignKey('Comment', on_delete=models.SET_NULL, null=True)  
     def __str__(self):
-        return f'{self.author} liked {self.comment}'
+        return f'{self.author} liked'
